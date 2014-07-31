e2cc3ae98ff3230c090135233c073e6fc71b371d
Daniel Miller <danm1113@gmail.com>
2014-07-30 20:40:20 -0700

    Issue #16. Added a lightsource model and basics for mapping out a lighting map.

--- actor.py
+++ actor.py
@@ -1,49 +1,65 @@
-#Imports
-#------------------------------------------------------------------------------
+# Imports
+# ------------------------------------------------------------------------------
 import libtcodpy as libtcod
-import lightSource as light
 
-#Classes
-#------------------------------------------------------------------------------
+# Classes
+# ------------------------------------------------------------------------------
+
+
 class Actor:
-	# Constructor
-	# Takes an x,y position with 0,0 being upper left corner; a single character,
-	# which is the symbolic representation of the actor; and the fore and back
-	# colors used to draw it.
-	#----------------------------------------------------------------------------
-	def __init__(self, x = 0, y = 0, symbol = '@', frontColor = libtcod.white,
-		backColor = libtcod.black):
-		self.x = x
-		self.y = y
-		self.symbol = symbol
-		self.frontColor = frontColor
-		self.backColor = backColor
-		self.parts = {"LightSource": None}
+    # Constructor
+    # Takes an x,y position with 0,0 being upper left corner; a single
+    # character which is the symbolic representation of the actor; and the
+    # fore and back colors used to draw it.
+    # -------------------------------------------------------------------------
 
-	# Draw function. Puts the symbol on the screen at its x,y position using
-	# its fore and back colors.
-	# Takes a libtcod console on which to draw on.
-	#----------------------------------------------------------------------------
-	def draw(self, console):
-		libtcod.console_put_char_ex(console, self.x, self.y, self.symbol,
-			self.frontColor, self.backColor)
+    def __init__(self, x=0, y=0, symbol='@', frontColor=libtcod.white,
+                 backColor=libtcod.black):
+        self.x = x
+        self.y = y
+        self.symbol = symbol
+        self.frontColor = frontColor
+        self.backColor = backColor
+        self.parts = {"LightSource": None}
 
-	# Movement function. Checks for nothing with X,Y so avoid passing bad values.
-	# Takes the new position of the Actor
-	def move(self, x, y):
-		self.x = x
-		self.y = y
+    # Draw function. Puts the symbol on the screen at its x,y position using
+    # its fore and back colors.
+    # Takes a libtcod console on which to draw on.
+    # -------------------------------------------------------------------------
+    def draw(self, console):
+        libtcod.console_put_char_ex(console, self.x, self.y, self.symbol,
+                                    self.frontColor, self.backColor)
 
-	# Add component function.
-	# Checks that the passed object isn't none, and then sets the object.parent
-	# back to this object, then calls the objects setup so it can construct
-	# things that it needs the parent actor for.
-	# Takes an object to compose onto the actor
-	def addComponent(self, composeObject):
-		if composeObject is None:
-			return
-		className = composeObject.__class__.__name__
-		if className in self.parts:
-			self.parts[className] = composeObject
-			composeObject.parent = self
-			composeObject.setup()
+    # Movement function. Checks for nothing with X,Y so avoid passing
+    # bad values.
+    # Takes the new position of the Actor
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
+    # Add component function.
+    # Checks that the passed object isn't none, and then sets the object.parent
+    # back to this object, then calls the objects setup so it can construct
+    # things that it needs the parent actor for.
+    # Takes an object to compose onto the actor
+    def addComponent(self, composeObject):
+        if composeObject is None:
+            return
+        className = composeObject.__class__.__name__
+        if className in self.parts:
+            self.parts[className] = composeObject
+            composeObject.parent = self
+            composeObject.setup()
+
+    # Register Actor function.
+    # Adds the actor into the engine's actor list.
+    # Takes nothing
+    def register(self):
+        # Need to import engine.
+        from engine import gEngine
+        # Add to the actor's list
+        gEngine.actors.append(self)
+        # Set our id to the newest ID
+        self.ID_ = gEngine.actor_counter
+        # Increment the available ID
+        gEngine.actor_counter += 1
--- game.py
+++ game.py
@@ -7,62 +7,69 @@ playerState = "idle"
 
 # Sets up for the program
 # -----------------------------------------------------------------------------
-def programSetup():
-	libtcod.console_set_custom_font(const.fontName,
-		libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
-	libtcod.sys_set_fps(const.fps)
-	libtcod.console_init_root(const.consoleWidth, const.consoleHeight,
-		'Stay in the Light')
-	libtcod.console_set_default_foreground(const.root, libtcod.white)
-	libtcod.console_clear(const.root)
-	gEngine.drawUILines()
 
+
+def programSetup():
+    libtcod.console_set_custom_font(
+        const.fontName,
+        libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+    libtcod.sys_set_fps(const.fps)
+    libtcod.console_init_root(const.consoleWidth, const.consoleHeight,
+                              'Stay in the Light')
+    libtcod.console_set_default_foreground(const.root, libtcod.white)
+    libtcod.console_clear(const.root)
+    gEngine.drawUILines()
 
 
 # MAIN GAME LOOP FUNCTION
 # -----------------------------------------------------------------------------
 def gameLoop():
-	while not libtcod.console_is_window_closed() and gameState != "gameDone":
-		gEngine.drawFrame()
-		key = libtcod.console_check_for_keypress(libtcod.KEY_PRESSED)
-		handleInput(key)
+    while not libtcod.console_is_window_closed() and gameState != "gameDone":
+        gEngine.drawFrame()
+        key = libtcod.console_check_for_keypress(libtcod.KEY_PRESSED)
+        handleInput(key)
+
 
 def funkyLight():
-	tiles = gEngine.findArea(22,30,9)
-	for tile in tiles:
-		tile.foreColor = libtcod.orange
-		tile.backColor = libtcod.blue
+    tiles = gEngine.findArea(22, 30, 9)
+    for tile in tiles:
+        tile.foreColor = libtcod.orange
+        tile.backColor = libtcod.blue
 
 # Player input handling (Temp, replaced once AI is implimented)
 # -----------------------------------------------------------------------------
+
+
 def handleInput(key):
-	global gameState
-	global playerState
-	if key.vk != libtcod.KEY_NONE:
-		cx = 0
-		cy = 0
-		if key.c == ord('4'):
-			cx -= 1
-			playerState = "moving"
-		elif key.c == ord('6'):
-			cx += 1
-			playerState = "moving"
-		elif key.c == ord('8'):
-			cy -= 1
-			playerState = "moving"
-		elif key.c == ord('2'):
-			cy += 1
-			playerState = "moving"
-		elif key.c == ord('5'):
-			playerState = "moving"
-		elif key.c == ord('q'):
-			gameState = "gameDone"
-		elif key.c == ord('l'):
-			funkyLight()
-		if(playerState == "moving"):
-			if( gEngine.isTileWalkable(cx + gEngine.player.x, cy + gEngine.player.y) ):
-				playerState = "moved"
-				gEngine.player.move(cx + gEngine.player.x, cy + gEngine.player.y)
-			else:
-				playerState = "idle"
-	return
+    global gameState
+    global playerState
+    if key.vk != libtcod.KEY_NONE:
+        cx = 0
+        cy = 0
+        if key.c == ord('4'):
+            cx -= 1
+            playerState = "moving"
+        elif key.c == ord('6'):
+            cx += 1
+            playerState = "moving"
+        elif key.c == ord('8'):
+            cy -= 1
+            playerState = "moving"
+        elif key.c == ord('2'):
+            cy += 1
+            playerState = "moving"
+        elif key.c == ord('5'):
+            playerState = "moving"
+        elif key.c == ord('q'):
+            gameState = "gameDone"
+        elif key.c == ord('l'):
+            funkyLight()
+        if(playerState == "moving"):
+            if(gEngine.isTileWalkable(cx + gEngine.player.x, cy +
+                                      gEngine.player.y)):
+                playerState = "moved"
+                gEngine.player.move(
+                    cx + gEngine.player.x, cy + gEngine.player.y)
+            else:
+                playerState = "idle"
+    return
